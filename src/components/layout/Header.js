fb72155d7853e2417c2add17d4f52b311fa2121c
Nicholas Fearns <you@example.com>
2019-09-16 20:11:05 +0000

    creating logo div and settings div

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,3 +1,21 @@
-import React from 'react'
+import React from 'react';  
 
-export const Header = () => <p>I am a header</p>;
+export const Header = () => {
+  const x =1
+
+  return <header className="header" data-testid="header">
+    <nav>
+      <div className="logo">
+        <img src="/images/logo.png" alt="Todoist" />
+      </div>
+
+      <div className="settings">
+        <ul>
+          <li>+</li>
+          <li>Pizza</li>
+        </ul>
+      </div>
+
+    </nav>
+
+  </header>;
