b0ba8ac5e0d4ee7ec4644cb10d22a475917e1320
Nicholas Fearns <you@example.com>
2019-09-19 21:46:08 +0000

    setting up sidebar component with divs for the different sections and used the imported icons.

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -2,16 +2,40 @@ import React from 'react';
 import {
   FaChevronDown,
   FaInbox,
-  FaRegCalenderAlt,
-  FaRegCalender,
+  FaRegCalendarAlt,
+  FaRegCalendar,
 } from 'react-icons/fa';
 
 export const Sidebar = () => (
 <div className="sidebar">
   <ul className="sidebar__generic">
-    <li>Inbox</li>
-    <li>Today</li>
-    <li>Next 7 Days</li>
+    <li>
+      <span>
+        <FaInbox /> 
+      </span>
+      <span>Inbox</span>
+    </li>
+      <li>
+      <span>
+        <FaRegCalendar /> 
+      </span>
+      <span>Today</span>
+    </li>
+    <li>
+      <span>
+        <FaRegCalendarAlt /> 
+      </span>
+      <span>Next 7 Days</span>
+    </li>
   </ul>
+
+  <div className="sidebar__middle">
+    <span>
+      <FaChevronDown />
+    </span>
+    <h2> Projects</h2>
+  </div>
+
+  <ul className="sidebar__projects">Projects will be here</ul>
 </div>
 ) ;
