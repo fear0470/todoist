bfc5c55309b9fa67c91ca4337ee2c5d8fd591d0e
Nicholas Fearns <you@example.com>
2019-09-19 21:55:17 +0000

    added firebase and node-sass as dependencies

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = firebase.initializeApp({
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messageSenderId: '',
+  apiId: '',
+})
