e3e53cb7d7174695681a4c5c6c4708905a70e5e5
Nicholas Fearns <you@example.com>
2019-09-19 21:31:46 +0000

    imported icons for sidebar

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,3 +1,17 @@
 import React from 'react';
+import {
+  FaChevronDown,
+  FaInbox,
+  FaRegCalenderAlt,
+  FaRegCalender,
+} from 'react-icons/fa';
 
-export const Sidebar = () => <div className="sidebar" data-test-id:"sidebar">
+export const Sidebar = () => (
+<div className="sidebar">
+  <ul className="sidebar__generic">
+    <li>Inbox</li>
+    <li>Today</li>
+    <li>Next 7 Days</li>
+  </ul>
+</div>
+) ;
