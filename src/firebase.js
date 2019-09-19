0a3e66d966c9f2d2cb98b386d30df62c457ffdd7
Nicholas Fearns <you@example.com>
2019-09-19 22:23:02 +0000

    setting up firebase database

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebasefrom 'firebase/app';
+import 'firebase/firestore;'
+
 const firebaseConfig = firebase.initializeApp({
   apiKey: '',
   authDomain: '',
@@ -6,4 +9,6 @@ const firebaseConfig = firebase.initializeApp({
   storageBucket: '',
   messageSenderId: '',
   apiId: '',
-})
+}) 
+
+export { firebaseConfig as firebase };
