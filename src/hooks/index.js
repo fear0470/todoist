b26beb502c79249407bec9702a9e48291b1176e5
Nicholas Fearns <you@example.com>
2019-09-22 20:18:47 +0000

    adding first hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -3,4 +3,11 @@ import { firebase} from '../firebase';
 
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
+
+  userEffect(() => {
+    let unsubscribe = firebase
+      .firestore()
+      .collection('tasks')
+      .where('userId', '==', 'cnccnw3');
+  }, []);
 }
