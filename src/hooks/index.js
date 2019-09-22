bcbd634ace2f48a95f61388e5e695e8d8d988dfd
Nicholas Fearns <you@example.com>
2019-09-22 23:23:04 +0000

    adding setTasks in the hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -25,5 +25,21 @@ export const useTasks = selectedProject => {
         :selectedProject === 'INBOX' || selectedProject === 0
         ? (unsubscribe = unsubscribe.where('date', '==', ''))
         : unsubscribe;
-  }, []);
+
+        unsubscribe = unsubscribe.onSnapshot(snapshot => {
+          const newTasks = snapshot.docs.map(task => ({
+            id: task.id,
+            ...task.data(), 
+          }));
+
+          setTasks(
+            selectedProject === 'NEXT_7'
+            ? newTasks.filter(
+                task => moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                task.archived !==true
+            )
+            : newTasks.filter(task => task.archived !==true)
+            )
+        });
+  }, [selectedProject]);
 }
