866203f3dc99c3dd58e08682df27d26006ac60ed
Nicholas Fearns <you@example.com>
2019-09-22 20:28:23 +0000

    working on tasks custom hook

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { firebase} from '../firebase';
 
+const collatedTasks = () => {};
+
 export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
 
@@ -9,5 +11,10 @@ export const useTasks = selectedProject => {
       .firestore()
       .collection('tasks')
       .where('userId', '==', 'cnccnw3');
+
+      unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ?
+        (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
+        : selectedProject === 'TODAY'
+        ? (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
   }, []);
 }
