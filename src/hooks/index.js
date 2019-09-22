7611854cad4fdf9fab45beef5dee8344062f709d
Nicholas Fearns <you@example.com>
2019-09-22 23:09:58 +0000

    added moment

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
+import moment from 'moment';
 import { firebase} from '../firebase';
+import { collatedTasksExist } from '../helpers';
 
 const collatedTasks = () => {};
 
@@ -15,6 +17,13 @@ export const useTasks = selectedProject => {
       unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ?
         (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
         : selectedProject === 'TODAY'
-        ? (unsubscribe = unsubscribe.where('date', '==', moment().format('DD/MM/YYYY')))
+        ? (unsubscribe = unsubscribe.where(
+          'date',
+          '==',
+          moment().format('DD/MM/YYYY')
+          ))
+        :selectedProject === 'INBOX' || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : unsubscribe;
   }, []);
 }
