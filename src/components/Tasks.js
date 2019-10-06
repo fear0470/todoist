e31083cb83fd021b8caff0f71615e5adf3232b73
Nicholas Fearns <you@example.com>
2019-10-06 22:32:03 +0000

    fixed errors in compilition, still working on getting the projects to show up. may be an issue with firestore.

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import { Checkbox } from "./Checkbox";
+import React, { useEffect } from 'react';
+import { Checkbox } from './Checkbox';
 import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-  const { tasks } = useTasks('1');
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
+
 
   let projectName = '';
+
+  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
+    projectName = getTitle(projects, selectedProject).name;
+  }
+
+  if (
+    projects &&
+    projects.length > 0 &&
+    selectedProject &&
+    !collatedTasksExist(selectedProject)
+    ) {
+      projectName = getTitle(projects, selectedProject).name;
+  }
+
+  useEffect(() => {
+    document.title = `${projectName}: TODOIST`;
+  });
 
   return ( 
     <div className="tasks" data-testid="tasks">
