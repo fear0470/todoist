3a06d0f6c3e497217db160594982588204eac8ac
Nicholas Fearns <you@example.com>
2019-10-10 21:09:16 +0000

    setting my const for the AddProject component

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -1,3 +1,8 @@
-import React from 'react'; 
+import React, { useState }  from 'react'; 
+import { firebase } from '../firebase';
+import { generatePushId } from '../helpers';
+import { useProjectsValue } from '../context';
 
-export const AddProject = () => <p> hello add project!</p>;
+export const AddProject = ({ shouldShow = false }) => {
+  const [, set] = useState();
+}
