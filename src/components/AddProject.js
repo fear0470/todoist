341691821fcd5deb4ec221e617ce4a0b7c13e3f3
Nicholas Fearns <you@example.com>
2019-10-10 21:37:29 +0000

    working on return for AddProject

--- src/components/AddProject.js
+++ src/components/AddProject.js
@@ -4,5 +4,71 @@ import { generatePushId } from '../helpers';
 import { useProjectsValue } from '../context';
 
 export const AddProject = ({ shouldShow = false }) => {
-  const [, set] = useState();
-}
+  const [show, setShow] = useState(shouldShow);
+  const [projectName, setProjectName] = useState('');
+
+  const projectId = generatePushId();
+  const { projects, setProjects } = useProjectsValue();
+
+  const AddProject = () =>
+    projectName &&
+    firebase
+      .firestore()
+      .collection('projects')
+      .add({
+        projectId,
+        name: projectName,
+        userId: 'cnccnw3',
+        })
+      .then(() => {
+        setProjects([...projects]);
+        setProjectName('');
+        setShow(false);
+      });
+
+  return (
+    <div className="add-project" data-testid="add-project">
+      {show && (
+        <div className="add-project__input" data-testid="add-project-inner">
+          <input
+            value={projectName}
+            onChange={e => setProjectName(e.target.value)}
+            className="add-project__name"
+            data-testid="project-name"
+            type="text"
+            placeholder="Name your project"
+            />
+            <button
+              className="add-project__submit"
+              type="button"
+              onClick={() => AddProject()}
+              data-testid="add-project-submit"
+            >
+              Add Project
+            </button>
+            <span
+              data-testid="hide-project-overlay"
+              className="add-project__cancel"
+              onClick={() => setShow(false)}
+              onKeyDown={() => setShow(false)}
+              role="button"
+              tabIndex={0}
+              >
+                Cancel
+              </span>
+        </div>
+      )}
+      <span className="add-project__plus">+</span>
+      <span
+        data-testid="add-project-action"
+        className="add-project__text"
+        onClick={() => setShow(!show)}
+        onKeyDown={() => setShow(!show)}
+        role="button"
+        tabIndex={0}
+        >
+          Add Project
+        </span>
+    </div>
+  );
+};
