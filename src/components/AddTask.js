3ce670d80a60eab1307e7d22de5a9f6f1afb7e7f
Nicholas Fearns <you@example.com>
2019-10-13 21:18:03 +0000

    more work on cancelation buttons

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -100,4 +100,39 @@ export const AddTask = ({
                 </div>
               </>
               )}
-              )}
+              <p>Project overlay here</p>
+              <p>TaskDate here</p>
+              <input
+                className="add-task__content"
+                data-testid="add-task-content"
+                type="text"
+                value={task}
+                onChange={e => setTask(e.target.value)}
+              />
+              <button
+                type="button"
+                className="add-task__submit"
+                data-testid="add-task"
+                onClick={() => addTask()}
+                >
+                  Add Task
+                </button>
+                {!showQuickAddTask && (
+                  <span
+                    className="add-task__cancel"
+                    data-testid="add-task-main-cancel"
+                    onClick={() => {
+                      setShowMain(false);
+                      setShowProjectOverlay(false);
+                    }}
+                    onKeyDown={() => {
+                      setShowMain(false);
+                      setShowProjectOverlay(false);
+                    }}
+                    tabIndex={0}
+                    role="button"
+                  >
+                    Cancel
+                  </span>
+                  )}
+            )}
