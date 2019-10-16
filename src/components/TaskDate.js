563d660513f9567789c38646332101cfa2a5ecea
Nicholas Fearns <you@example.com>
2019-10-16 23:54:03 +0000

    added to TaskDate export statement and added the imports, including the needed font awesome icons.

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -1,3 +1,21 @@
 import React from 'react';
+import moment from 'moment';
+import { FaSpaceShuttle, FaSun, FaRegPaperPlane } from 'react-icons/fa';
 
-export const TaskDate = () => <p>Hello</p>;
+export const TaskDate = ({ setTaskDate,showTaskDate, setShowTaskDate }) => 
+  showTaskDate && (
+    <div className="task-date" data-testid="task-date-overlay">
+      <ul className="task-date__list">
+        <div
+          onClick={() => {
+            setShowTaskDate(false);
+            setTaskDate(moment().format('DD/MM/YYYY'));
+          }}
+          data-testid="task-date-today"
+          tabIndex={0}
+          role="button"
+        >
+          <span>
+            <FaSpaceShuttle />
+
+        )
