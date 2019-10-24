84b6779a75426deca3aa38d4fdffa39b62586905
Nicholas Fearns <you@example.com>
2019-10-24 00:19:32 +0000

    created mock call to firebase using jest

--- src/__tests__/Checkbox.spec.js
+++ src/__tests__/Checkbox.spec.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { CheckBox } from '../components/Checkbox';
+import { JestEnvironment } from '@jest/environment';
 
+beforeEach(cleanup);
 
+jest.mock('../firebase', () => ({
+    firebase: {
+        firestore: jest.fn(() => ({
+            collection: jest.fn(() => ({
+                doc: jest.fn(() => ({
+                    update: jest.fn(),
+                })),
+            })),
+        })),
+    },
+}));
