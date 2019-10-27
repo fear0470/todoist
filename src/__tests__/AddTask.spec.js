102ded8d677583d286ecf7a08e53d91d04626493
Nicholas Fearns <you@example.com>
2019-10-27 22:04:22 +0000

    added tests to see if AddTask is rendering

--- src/__tests__/AddTask.spec.js
+++ src/__tests__/AddTask.spec.js
@@ -5,3 +5,40 @@ import { useSelectedProjectValue } from '../context';
 
 beforeEach(cleanup);
 
+JestEnvironment.mock('../firebase', () => ({
+    firebase: {
+        firestore: jest.fn(() => ({
+            collection: jest.fn(() => ({
+                add: jest.fn(() => Promise.resolve('Never mock firebase')),
+            })),
+        })),
+    },
+}));
+
+describe('<AddTask />', () => {
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    describe('Success', () => {
+        it('renders the <AddTask />', () => {
+            const { queryByTestId } = render(<AddTask />);
+            expect(queryByTestId('add-task-comp')).toBeTruthy();
+        });
+
+        it('renders the <AddTask /> quick overlay', () => {
+            const setShowQuickAddTask = jest.fn();
+
+            const { queryByTestId } = render(
+                <AddTask
+                    showAddTaskMain
+                    shouldShowMain={false}
+                    showQuickAddTask
+                    setShowQuickAddTask={setShowQuickAddTask}
+                />
+            );
+        })
+
+        expect(queryByTestId('quick-add-task')).toBeTruthy();
+    });
+})
