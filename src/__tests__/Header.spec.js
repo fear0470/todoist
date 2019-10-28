d81b86f86b74787fd26c302532b6645e801dd04d
Nicholas Fearns <you@example.com>
2019-10-28 21:38:59 +0000

    testing darkmode in the header component

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from '@testing-library/react';
 import { Header } from '../components/layout/Header';
-import { JestEnvironment } from '@jest/environment';
 
 Jest.mock('../context', () => ({
     useSelectedProjectValue: jest.fn(() => ({ selectedProject: 1})),
@@ -9,3 +8,40 @@ Jest.mock('../context', () => ({
 }));
 
 beforeEach(cleanup);
+
+describe('<Header />', () => {
+    describe('Success', () => {
+        it('renders the header component', () => {
+            const { queryByTestId } = render(<Header />);
+            expect(queryByTestId('header')).toBeTruthy();
+        });
+
+        it('renders the header component and activates dark mode using onClick', () => {
+            const darkMode = false;
+            const setDarkMode = jest.fn(() => !darkMode);
+
+            const { queryByTestId } = render(
+                <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            );
+            expect(queryByTestId('header')).toBeTruthy();
+            
+            fireEvent.click(queryByTestId('dark-mode-action'));
+            expect(setDarkMode).toHaveBeenCalledWith(true);
+        });
+
+        it('renders the header component and activates dark mode using onKeyDown', () => {
+            const darkMode = false;
+            const setDarkMode = jest.fn(() => !darkMode);
+
+            const { queryByTestId } = render(
+                <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+            );
+            expect(queryByTestId('header')).toBeTruthy();
+            
+            fireEvent.keyDown(queryByTestId('dark-mode-action'));
+            expect(setDarkMode).toHaveBeenCalledWith(true);
+
+            fireEvent.keyDown(queryByTestId('dark-mode-action'));
+            expect(setDarkMode).toHaveBeenCalledTimes(2);
+        });
+})
