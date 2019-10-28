4af4ed16cc5b5b810987c6cafcfd50f825454258
Nicholas Fearns <you@example.com>
2019-10-28 21:44:56 +0000

    more tests for the header component

--- src/__tests__/Header.spec.js
+++ src/__tests__/Header.spec.js
@@ -44,4 +44,24 @@ describe('<Header />', () => {
             fireEvent.keyDown(queryByTestId('dark-mode-action'));
             expect(setDarkMode).toHaveBeenCalledTimes(2);
         });
+
+        it('renders the header component and set quick add task to true using onClick', () => {
+            const darkMode = false;
+
+            const { queryByTestId } = render(<Header darkMode={darkMode} />);
+            expect(queryByTestId('header')).toBeTruthy();
+
+            fireEvent.click(queryByTestId('quick-add-task-action'));
+            expect(queryByTestId('add-task-main')).toBeTruthy();
+        });
+
+        it('renders the header component and set quick add task to true using onKeyDown', () => {
+            const darkMode = false;
+
+            const { queryByTestId } = render(<Header darkMode={darkMode} />);
+            expect(queryByTestId('header')).toBeTruthy();
+
+            fireEvent.keyDown(queryByTestId('quick-add-task-action'));
+            expect(queryByTestId('add-task-main')).toBeTruthy();
+        });
 })
