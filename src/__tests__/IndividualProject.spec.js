1d4f3c1d02783719bb7f08d6562de19bb64a3f25
Nicholas Fearns <you@example.com>
2019-11-02 23:08:46 +0000

    added IndividualPRoject.spec.js and added imports, set up the mock and the cleanup

---
+++ src/__tests__/IndividualProject.spec.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render, fireEvent, cleanup } from '@testing-library/react';
+import { IndividualProject } from '../components/IndividualProject';
+
+beforeEach(cleanup);
